caeb5c97a3e61e73b8c4f19d2c949bba463148aa
davidbtr <161590165+davidbtr@users.noreply.github.com>
2024-03-27 16:51:38 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ function door(){
                 clearTimeout(esperaRedireccion);
                 esperaRedireccion = setTimeout(() => {
                     window.location.href = "https://elparca.com/door.html";
-                }, 100);
+                }, 200);
             } else {
                 clearTimeout(esperaRedireccion);
             }
